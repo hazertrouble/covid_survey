1a8c3533959a97eaaa7fd4253fa2d4ab7d68bb27
Omar Ortega <srhazer.page@gmail.com>
2022-10-16 10:59:17 -0700

    sweet alerts added

--- survey.js
+++ survey.js
@@ -4,6 +4,17 @@ const form = document.forms['submit-form']
 form.addEventListener('submit', e => {
   e.preventDefault()
   fetch(scriptURL, { method: 'POST', body: new FormData(form)})
-    .then(response => console.log('Success!', response))
-    .catch(error => console.error('Error!', error.message))
+    .then(response => {
+      console.log('Success!', response);
+      swal("Good job!", "Thanks for answering the survey", "success");
+    })
+    .catch(error => {
+      console.error('Error!', error.message);
+      swal({
+        title: "Error!",
+        text: "Here's my error message!",
+        type: "error",
+        confirmButtonText: "Cool"
+      });
+    })
 })
